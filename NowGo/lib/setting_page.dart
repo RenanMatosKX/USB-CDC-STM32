f7d78886386e691916d95428bc9d01681aad7884
RenanMatosKX <renan.matos@karimex.com.br>
2023-02-28 09:04:29 -0300

    Finished the page setting widget

--- NowGo/lib/setting_page.dart
+++ NowGo/lib/setting_page.dart
@@ -18,8 +18,14 @@ String dateDay = "17";
 String dateMonth = "02";
 String dateYear = "2023";
 
-final dropValue = ValueNotifier('');
-final dropBaudRate = ['2400','9600','19200','38400','57600','115200'];
+final dropValue1 = ValueNotifier('');
+final dropValue2 = ValueNotifier('');
+final dropValue3 = ValueNotifier('');
+final dropValue4 = ValueNotifier('');
+final dropBaudRate = ['2400', '9600', '19200', '38400', '57600', '115200'];
+final dropDatabits = ['5', '6', '7', '8'];
+final dropParity = ['None', 'Odd', 'Even'];
+final dropStopBits = ['1', '1.5', '2'];
 
 class SettingPage extends StatefulWidget {
   const SettingPage({super.key});
@@ -41,116 +47,203 @@ class _SettingPageState extends State<SettingPage> {
         foregroundColor: Colors.white,
       ),
 
-/*
       body: Column(
         children: [
-         //Item 1 - Led 12 
-          ListTile(
-            title: Text('Baud rate'),
-            subtitle: stateLED12 ? Text("115200") : Text('9600'),
-            leading: const Icon(
-              Icons.emoji_objects,
-              size: 60.0,
+          //Label Data bits
+          SizedBox(
+            width: 320,
+            height: 50,
+            child: Row(
+              children: [
+                Text('Baud Rate:',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20)),
+              ],
             ),
-            trailing: const Icon(
-              Icons.power_settings_new,
-              size: 50.0,
-            ),
-            //iconColor: Colors.amber,
-            iconColor: stateLED12 ? Colors.grey : Colors.greenAccent,
-
-            onTap: () {
-              setState(() => stateLED12 = !stateLED12);
-              debugPrint('Item ${(1 + 1)}');
-            },
           ),
-         //Item 2 - LED 13
-          ListTile(
-            title: Text('Data bits'),
-             subtitle: stateLED13 ? Text("Desligado") : Text('Ligado'),
-            leading: const Icon(
-              Icons.emoji_objects,
-              size: 60.0,
-            ),
-            trailing: const Icon(
-              Icons.power_settings_new,
-              size: 50.0,
-            ),
-            iconColor: stateLED13 ? Colors.grey : Colors.orangeAccent,
-            onTap: () {
-              setState(() => stateLED13 = !stateLED13);
-              debugPrint('Item ${(1 + 1)}');
-            },
-          ),
-         // Item 3 - LED 14
-          ListTile(
-            title: Text('Parity'),
-             subtitle: stateLED14 ? Text("Desligado") : Text('Ligado'),
-            leading: const Icon(
-              Icons.emoji_objects,
-              size: 60.0,
-            ),
-            trailing: const Icon(
-              Icons.power_settings_new,
-              size: 50.0,
-            ),
-              iconColor: stateLED14 ? Colors.grey : Colors.redAccent,
-            onTap: () {
-               setState(() => stateLED14 = !stateLED14);
-              debugPrint('Item ${(1 + 1)}');
-            },
-          ),
-         
-         // Item 4 - LED 15 
-          ListTile(
-            title: Text('Stop bits'),
-           subtitle: stateLED15 ? Text("Desligado") : Text('Ligado'),
-            leading: const Icon(
-              Icons.emoji_objects,
-              size: 60.0,
-            ),
-            trailing: const Icon(
-              Icons.power_settings_new,
-              size: 50.0,
-            ),
-              iconColor: stateLED15 ? Colors.grey : Colors.lightBlue,
-            onTap: () {
-               setState(() => stateLED15 = !stateLED15);
-              debugPrint('Item ${(1 + 1)}');
-            },
+          //DropDownMenu BaudRate
+          Center(
+                        heightFactor: 0.7,
+            child: ValueListenableBuilder(
+                valueListenable: dropValue1,
+                builder: (BuildContext context, String value, _) {
+                  return SizedBox(
+                    width: 320,
+                    child: DropdownButtonFormField<String>(
+                      isExpanded: true,
+                      icon: const Icon(Icons.arrow_drop_down),
+                      hint: const Text('9600'),
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(6),
+                      )),
+                      value: (value.isEmpty) ? null : value,
+                      onChanged: (escolha) =>
+                          dropValue1.value = escolha.toString(),
+                      items: dropBaudRate
+                          .map(
+                            (op) => DropdownMenuItem(
+                              value: op,
+                              child: Text(op),
+                            ),
+                          )
+                          .toList(),
+                    ),
+                  );
+                }),
           ),
 
+          //Label Data bits
+          SizedBox(
+            width: 320,
+            height: 50,
+            child: Row(
+              children: [
+                Text('Data bits:',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20)),
+              ],
+            ),
+          ),
+          //DropDownMenu Data bits
+          Center(
+            heightFactor: 0.7,
+            child: ValueListenableBuilder(
+                valueListenable: dropValue2,
+                builder: (BuildContext context, String value, _) {
+                  return SizedBox(
+                    width: 320,
+                    child: DropdownButtonFormField<String>(
+                      isExpanded: true,
+                      icon: const Icon(Icons.arrow_drop_down),
+                      hint: const Text('8'),
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(6),
+                      )),
+                      value: (value.isEmpty) ? null : value,
+                      onChanged: (escolha) =>
+                          dropValue2.value = escolha.toString(),
+                      items: dropDatabits
+                          .map(
+                            (op) => DropdownMenuItem(
+                              value: op,
+                              child: Text(op),
+                            ),
+                          )
+                          .toList(),
+                    ),
+                  );
+                }),
+          ),
 
-        ],   
+          //Label Parity
+          SizedBox(
+            width: 320,
+            height: 50,
+            child: Row(
+              children: [
+                Text('Parity: ',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20)),
+              ],
+            ),
+          ),
+          //DropDownMenu Data bits
+          Center(
+            heightFactor: 0.7,
+            child: ValueListenableBuilder(
+                valueListenable: dropValue3,
+                builder: (BuildContext context, String value, _) {
+                  return SizedBox(
+                    width: 320,
+                    child: DropdownButtonFormField<String>(
+                      isExpanded: true,
+                      icon: const Icon(Icons.arrow_drop_down),
+                      hint: const Text('None'),
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(6),
+                      )),
+                      value: (value.isEmpty) ? null : value,
+                      onChanged: (escolha) =>
+                          dropValue3.value = escolha.toString(),
+                      items: dropParity
+                          .map(
+                            (op) => DropdownMenuItem(
+                              value: op,
+                              child: Text(op),
+                            ),
+                          )
+                          .toList(),
+                    ),
+                  );
+                }),
+          ),
 
+          //Label Stop Bits
+          SizedBox(
+            width: 320,
+            height: 50,
+            child: Row(
+              children: [
+                Text('Stop bits:',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20)),
+              ],
+            ),
+          ),
+          //DropDownMenu Data bits
+          Center(
+                        heightFactor: 0.7,
+            child: ValueListenableBuilder(
+                valueListenable: dropValue4,
+                builder: (BuildContext context, String value, _) {
+                  return SizedBox(
+                    width: 320,
+                    child: DropdownButtonFormField<String>(
+                      isExpanded: true,
+                      icon: const Icon(Icons.arrow_drop_down),
+                      hint: const Text('8'),
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(6),
+                      )),
+                      value: (value.isEmpty) ? null : value,
+                      onChanged: (escolha) =>
+                          dropValue4.value = escolha.toString(),
+                      items: dropStopBits
+                          .map(
+                            (op) => DropdownMenuItem(
+                              value: op,
+                              child: Text(op),
+                            ),
+                          )
+                          .toList(),
+                    ),
+                  );
+                }),
+          ),
+
+          Center(
+            heightFactor: 2,
+            child: TextButton(
+                style: ButtonStyle(
+                    fixedSize: MaterialStateProperty.all<Size>(Size(200, 70)),
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.blue)),
+                onPressed: () {
+                  if(dropValue1.value == '9600'){
+                      debugPrint('Baud Rate Selected : 9600');
+                  }
+                },
+                child:
+                 Text('Save Settings')),
+          ),
+        ],
       ),
-*/
-      body: Center(
-        child: ValueListenableBuilder(
-          valueListenable: dropValue, builder:(BuildContext context, String value, _){
-              return SizedBox(
-                width: 320,
-                child: DropdownButtonFormField<String>(
-                  
-                isExpanded: true,
-                 icon: const Icon(Icons.drive_eta),
-                 hint: const Text('9600'),
-                 decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(6),
-                    )
-                 ) ,
-                  value: (value.isEmpty) ? null:value,
-                  onChanged: (escolha) => dropValue.value = escolha.toString(),
-                  items: dropBaudRate.map((op) => DropdownMenuItem(
-                    value: op,
-                    child: Text(op),
-                    ),).toList(),
-                 ),
-              );
-          }),
-         ),
-
 
       //Menu Lateral
       drawer: Drawer(
